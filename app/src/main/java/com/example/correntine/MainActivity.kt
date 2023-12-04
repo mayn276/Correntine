57f1ff96f82250553b51a0405a92bdc48e4915fa
Mayank Sharma <Ms1202864@gmail.com>
2023-12-04 16:38:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/correntine/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.correntine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    private lateinit var messageTextview : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tvCount)
+        messageTextview = findViewById(R.id.tvMessage)
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        countButton.setOnClickListener {
+            textView.text = count++.toString()
+        }
+        downloadButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+
+            }
+        }
+    }
+
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                messageTextview.text ="Downloading user $i"
+            }
+            delay(100)
+        }
+    }
+}
+
